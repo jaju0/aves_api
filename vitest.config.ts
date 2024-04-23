714d518f5ec6c3e05aa20c4d46ba3ee8e70ba6c0
jaju0 <jannes_oltmanns@hotmail.de>
2024-04-23 11:27:46 +0200

    added vitest config file

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+import { configDefaults, defineConfig } from "vitest/config";
+
+export default defineConfig({
+    plugins: [],
+    test: {
+        exclude: [
+            ...configDefaults.exclude,
+            "dist/*",
+            "dist/**/*",
+        ],
+    },
+});
