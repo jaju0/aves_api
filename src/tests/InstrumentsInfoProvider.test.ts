c127dbd67c0b53360c94d4223c88a85a04f2afcf
jaju0 <jannes_oltmanns@hotmail.de>
2024-04-23 19:45:01 +0200

    added InstrumentsInfoProvider

---
+++ src/tests/InstrumentsInfoProvider.test.ts
@@ -0,0 +1,12 @@
+import { expect, test } from "vitest";
+import { RestClientV5 } from "bybit-api";
+import { InstrumentsInfoProvider } from "../InstrumentsInfoProvider.js";
+
+test("InstrumentInfoProvider", async () => {
+    const refreshIntervalMs = 24 * 60 * 60 * 1000;
+    const restClient = new RestClientV5();
+    const instInfoProvider = new InstrumentsInfoProvider(restClient, refreshIntervalMs);
+
+    const instInfo = await instInfoProvider.get("BTCUSDT");
+    expect(instInfo).toBeDefined();
+});
