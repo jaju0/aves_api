2b8df090fb46fbc4a542f9d63d5e6061a946d4d2
jaju0 <jannes_oltmanns@hotmail.de>
2024-04-26 22:07:52 +0200

    added position model

---
+++ src/models/Position.ts
@@ -0,0 +1,37 @@
+import mongoose, { Model } from "mongoose";
+
+export type PositionSide = "None" | "Long" | "Short";
+
+export interface IPosition
+{
+    side: PositionSide;
+    symbol1: string;
+    symbol2: string;
+    symbol1EntryPrice: string;
+    symbol2EntryPrice: string;
+    symbol1BaseQty: string;
+    symbol2BaseQty: string;
+    regressionSlope: string;
+    takeProfit?: string;
+    stopLoss?: string;
+    open: boolean;
+}
+
+type PositionModel = Model<IPosition>;
+
+const positionSchema = new mongoose.Schema<IPosition, PositionModel>({
+    side: { type: String, required: true },
+    symbol1: { type: String, required: true },
+    symbol2: { type: String, required: true },
+    symbol1EntryPrice: { type: String, required: true },
+    symbol2EntryPrice: { type: String, required: true },
+    symbol1BaseQty: { type: String, required: true },
+    symbol2BaseQty: { type: String, required: true },
+    regressionSlope: { type: String, required: true },
+    takeProfit: { type: String, required: false },
+    stopLoss: { type: String, required: false },
+    open: { type: Boolean, required: true, default: true },
+});
+
+export const Position = mongoose.model("Position", positionSchema);
+export type Position = InstanceType<typeof Position>;
