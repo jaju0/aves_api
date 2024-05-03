ccd4c3663996f555d498e0d36440ba6251434398
jaju0 <jannes_oltmanns@hotmail.de>
2024-05-04 01:07:04 +0200

    added user model

---
+++ src/models/User.ts
@@ -0,0 +1,22 @@
+import mongoose, { Model } from "mongoose";
+import { Credential } from "./Credential.js";
+
+export interface IUser
+{
+    google_id: string;
+    email: string;
+    credentials: Credential[];
+    active_credential: Credential;
+}
+
+type UserModel = Model<IUser>;
+
+const userSchema = new mongoose.Schema<IUser, UserModel>({
+    google_id: { type: String, required: false },
+    email: { type: String, required: true },
+    credentials: [{ type: mongoose.Schema.Types.ObjectId, ref: "Credential", required: true }],
+    active_credential: { type: mongoose.Schema.Types.ObjectId, ref: "Credential", required: false },
+});
+
+export const User = mongoose.model("User", userSchema);
+export type User = InstanceType<typeof User>;
