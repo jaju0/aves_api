67a02697f50fc9c16bb05aa4b61ab0d39453949c
jaju0 <jannes_oltmanns@hotmail.de>
2024-05-07 01:05:18 +0200

    added order list and amendment handlers

--- src/routes/v1/order/controllers.ts
+++ src/routes/v1/order/controllers.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { OrderSide, OrderType } from "../../../models/Order.js";
+import { Order, OrderSide, OrderType } from "../../../models/Order.js";
 import { Credential } from "../../../models/Credential.js";
 import { OrderCoordinatorProvider } from "../../../core/OrderCoordinatorProvider.js";
 
@@ -7,6 +7,24 @@ export interface BaseQty
 {
     symbol1BaseQty: number;
     symbol2BaseQty: number;
+}
+
+export interface OrderData
+{
+    orderId: string;
+    type: OrderType;
+    side: OrderSide;
+    symbol1: string;
+    symbol2: string;
+    symbol1BaseQty: string;
+    symbol2BaseQty: string;
+    quoteQty?: string;
+    entryResidual?: string;
+    regressionSlope: string;
+    takeProfit?: string;
+    stopLoss?: string;
+    executed: boolean;
+    failed: boolean;
 }
 
 export interface OrderSubmitionRequest
@@ -26,6 +44,34 @@ export interface OrderSubmitionRequest
 export interface OrderSubmitionResponse
 {
     orderId: string;
+}
+
+export interface OrderAmendmentRequest
+{
+    orderId: string;
+    entryResidual: number;
+}
+
+export type OrderListResponse = OrderData[];
+
+function orderDocumentToResponseData(order: Order)
+{
+    return <OrderData> {
+        orderId: order.id,
+        type: order.type,
+        side: order.side,
+        symbol1: order.symbol1,
+        symbol2: order.symbol2,
+        symbol1BaseQty: order.symbol1BaseQty,
+        symbol2BaseQty: order.symbol2BaseQty,
+        quoteQty: order.quoteQty,
+        entryResidual: order.entryResidual,
+        regressionSlope: order.regressionSlope,
+        takeProfit: order.takeProfit,
+        stopLoss: order.stopLoss,
+        executed: order.executed,
+        failed: order.failed,
+    };
 }
 
 export async function orderSubmitionHandler(orderCoordinatorProvider: OrderCoordinatorProvider, req: Request<any, any, OrderSubmitionRequest>, res: Response<OrderSubmitionResponse>)
@@ -60,6 +106,8 @@ export async function orderSubmitionHandler(orderCoordinatorProvider: OrderCoord
         orderId: orderContext.order.id,
     });
 }
+
+export async function orderAmendmentHandler(orderCoordinatorProvider: OrderCoordinatorProvider, req: Request<any, any, OrderAmendmentRequest>, res: Response)
 {
     if(req.user === undefined)
         return res.sendStatus(401);
@@ -73,7 +121,30 @@ export async function orderSubmitionHandler(orderCoordinatorProvider: OrderCoord
         return res.sendStatus(400);
 
     const orderCoordinator = orderCoordinatorProvider.get(activeCredential.key, activeCredential.secret, activeCredential.demoTrading);
-    orderCoordinator.createOrder(req.body); // <--- TODO: get the order data from here
+    const orderData = orderCoordinator.getOrderData(req.body.orderId);
+    if(orderData === undefined)
+        return res.sendStatus(404);
 
-    return res.sendStatus(200); // <--- TODO: pass the order data received from orderCoordinator.createOrder() here
+    orderData.amendEntryResidual(data.entryResidual);
+    return res.sendStatus(200);
+}
+
+export async function orderListHandler(orderCoordinatorProvider: OrderCoordinatorProvider, req: Request, res: Response<OrderListResponse>)
+{
+    if(req.user === undefined)
+        return res.sendStatus(401);
+
+    const activeCredential = await Credential.getActiveCredential();
+    if(activeCredential === "error")
+        return res.sendStatus(500);
+    else if(activeCredential == undefined)
+        return res.sendStatus(400);
+
+    const orderCoordinator = orderCoordinatorProvider.get(activeCredential.key, activeCredential.secret, activeCredential.demoTrading);
+
+    const activeOrderList = new Array<OrderData>();
+    for(const context of orderCoordinator.OrderContexts.values())
+        activeOrderList.push(orderDocumentToResponseData(context.order));
+
+    return res.json(activeOrderList);
 }
