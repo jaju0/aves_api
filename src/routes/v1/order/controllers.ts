b0f4f8206c05b12e422af1f60728d1add0dda02c
jaju0 <jannes_oltmanns@hotmail.de>
2024-05-07 01:04:38 +0200

    changed order submition handler implementation

--- src/routes/v1/order/controllers.ts
+++ src/routes/v1/order/controllers.ts
@@ -9,21 +9,57 @@ export interface BaseQty
     symbol2BaseQty: number;
 }
 
-export interface OrderSubmitionResponse
+export interface OrderSubmitionRequest
 {
     type: OrderType;
     side: OrderSide;
     symbol1: string;
     symbol2: string;
     regressionSlope: number;
-    entryResidual: number;
+    entryResidual?: number;
     takeProfit?: number;
     stopLoss?: number;
     quoteQty?: number;
     baseQty?: BaseQty;
 }
 
-export async function orderSubmitionHandler(orderCoordinatorProvider: OrderCoordinatorProvider, req: Request, res: Response)
+export interface OrderSubmitionResponse
+{
+    orderId: string;
+}
+
+export async function orderSubmitionHandler(orderCoordinatorProvider: OrderCoordinatorProvider, req: Request<any, any, OrderSubmitionRequest>, res: Response<OrderSubmitionResponse>)
+{
+    if(req.user === undefined)
+        return res.sendStatus(401);
+
+    const data = req.body;
+
+    const activeCredential = await Credential.getActiveCredential();
+    if(activeCredential === "error")
+        return res.sendStatus(500);
+    else if(activeCredential == undefined)
+        return res.sendStatus(400);
+
+    const orderCoordinator = orderCoordinatorProvider.get(activeCredential.key, activeCredential.secret, activeCredential.demoTrading);
+
+    const orderContext = await orderCoordinator.createOrder({
+        type: req.body.type,
+        side: req.body.side,
+        symbol1: req.body.symbol1,
+        symbol2: req.body.symbol2,
+        regressionSlope: req.body.regressionSlope.toString(),
+        entryResidual: req.body.entryResidual?.toString(),
+        takeProfit: req.body.takeProfit?.toString(),
+        stopLoss: req.body.stopLoss?.toString(),
+        symbol1BaseQty: req.body.baseQty?.symbol1BaseQty.toString(),
+        symbol2BaseQty: req.body.baseQty?.symbol2BaseQty.toString(),
+    });
+
+    return res.json({
+        orderId: orderContext.order.id,
+    });
+}
 {
     if(req.user === undefined)
         return res.sendStatus(401);
