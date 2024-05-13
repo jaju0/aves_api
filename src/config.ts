d44f751e34d2d2ea18a1532ccc2f37e9be876b58
jaju0 <jannes_oltmanns@hotmail.de>
2024-05-14 01:46:34 +0200

    added mongoose credentials & pnl update interval env variables

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,10 @@ declare global
             SESSION_SECRET: string;
             JSON_WEB_TOKEN_SECRET: string;
             JSON_WEB_TOKEN_EXPIRES_IN: string;
+            MONGOOSE_USER: string;
+            MONGOOSE_PASS: string;
             BYBIT_INSTRUMENTS_INFO_REFETCH_INTERVAL_HOURS: string;
+            POSITION_PNL_UPDATE_INTERVAL_MS: string;
         }
     }
 }
@@ -20,7 +23,10 @@ const config = {
     SESSION_SECRET: process.env.SESSION_SECRET,
     JSON_WEB_TOKEN_SECRET: process.env.JSON_WEB_TOKEN_SECRET,
     JSON_WEB_TOKEN_EXPIRES_IN: process.env.JSON_WEB_TOKEN_EXPIRES_IN === undefined ? 24 * 60 * 60 : +process.env.JSON_WEB_TOKEN_EXPIRES_IN,
+    MONGOOSE_USER: process.env.MONGOOSE_USER,
+    MONGOOSE_PASS: process.env.MONGOOSE_PASS,
     BYBIT_INSTRUMENTS_INFO_REFETCH_INTERVAL_HOURS: process.env.BYBIT_INSTRUMENTS_INFO_REFETCH_INTERVAL_HOURS === undefined ? 24 : +process.env.BYBIT_INSTRUMENTS_INFO_REFETCH_INTERVAL_HOURS,
+    POSITION_PNL_UPDATE_INTERVAL_MS: process.env.POSITION_PNL_UPDATE_INTERVAL_MS === undefined ? 5000 : +process.env.POSITION_PNL_UPDATE_INTERVAL_MS,
 };
 
 if(config.SESSION_SECRET === undefined)
@@ -28,5 +34,11 @@ if(config.SESSION_SECRET === undefined)
 
 if(config.JSON_WEB_TOKEN_SECRET === undefined)
     throw new Error("environment variable JSON_WEB_TOKEN_SECRET is undefined");
+
+if(config.MONGOOSE_USER === undefined)
+    throw new Error("environment variable MONGOOSE_USER is undefined");
+
+if(config.MONGOOSE_PASS === undefined)
+    throw new Error("environment variable MONGOOSE_PASS is undefined");
 
 export default config;
