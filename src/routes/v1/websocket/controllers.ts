7906c7d17012572379f9c6e96acf5785723e6ca6
jaju0 <jannes_oltmanns@hotmail.de>
2024-05-14 23:43:37 +0200

    added exception handling for websocket authentication

--- src/routes/v1/websocket/controllers.ts
+++ src/routes/v1/websocket/controllers.ts
@@ -6,7 +6,15 @@ import { websocketAuthenticate } from "../../../middleware/websocketAuthenticate
 
 export async function websocketHandler(websocketAgentProvider: WebsocketAgentProvider, ws: WebSocket, req: Request, next: NextFunction)
 {
-    await websocketAuthenticate("jwt-query", { session: false }, ws, req, next);
+    try
+    {
+        await websocketAuthenticate("jwt-query", { session: false }, ws, req, next);
+    }
+    catch(error)
+    {
+        ws.close(1008);
+        return;
+    }
 
     if(req.user === undefined)
     {
