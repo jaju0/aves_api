3ebf05f261028c7c82c9ca95ee69535e16933997
jaju0 <jannes_oltmanns@hotmail.de>
2024-05-16 23:43:00 +0200

    added MONGODB_PORT env variable

--- src/config.ts
+++ src/config.ts
@@ -10,8 +10,10 @@ declare global
             SESSION_SECRET: string;
             JSON_WEB_TOKEN_SECRET: string;
             JSON_WEB_TOKEN_EXPIRES_IN: string;
-            MONGOOSE_USER: string;
-            MONGOOSE_PASS: string;
+            MONGODB_PORT: string;
+            MONGODB_DB_NAME: string;
+            MONGOOSE_USER?: string;
+            MONGOOSE_PASS?: string;
             BYBIT_INSTRUMENTS_INFO_REFETCH_INTERVAL_HOURS: string;
             POSITION_PNL_UPDATE_INTERVAL_MS: string;
         }
@@ -23,6 +25,8 @@ const config = {
     SESSION_SECRET: process.env.SESSION_SECRET,
     JSON_WEB_TOKEN_SECRET: process.env.JSON_WEB_TOKEN_SECRET,
     JSON_WEB_TOKEN_EXPIRES_IN: process.env.JSON_WEB_TOKEN_EXPIRES_IN === undefined ? 24 * 60 * 60 : +process.env.JSON_WEB_TOKEN_EXPIRES_IN,
+    MONGODB_PORT: process.env.MONGODB_PORT === undefined ? 27017 : +process.env.MONGODB_PORT,
+    MONGODB_DB_NAME: process.env.MONGODB_DB_NAME ?? "aves",
     MONGOOSE_USER: process.env.MONGOOSE_USER,
     MONGOOSE_PASS: process.env.MONGOOSE_PASS,
     BYBIT_INSTRUMENTS_INFO_REFETCH_INTERVAL_HOURS: process.env.BYBIT_INSTRUMENTS_INFO_REFETCH_INTERVAL_HOURS === undefined ? 24 : +process.env.BYBIT_INSTRUMENTS_INFO_REFETCH_INTERVAL_HOURS,
